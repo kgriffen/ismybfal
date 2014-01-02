ad6c8408d4b718a6b70642e8f44c811efeffc6cc
kgriffen <karn.griffen@gmail.com>
2014-01-02 11:44:02 -0800

    Initial

---
+++ ISMYBFAL/mainViewController.h
@@ -0,0 +1,15 @@
+//
+//  mainViewController.h
+//  ISMYBFAL
+//
+//  Created by Karn Griffen on 1/2/14.
+//  Copyright (c) 2014 Karn Griffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "sqlite3.h"
+#import "FMDatabase.h"
+
+@interface mainViewController : UIViewController
+
+@end
---
+++ ISMYBFAL/mainViewController.m
@@ -0,0 +1,40 @@
+//
+//  mainViewController.m
+//  ISMYBFAL
+//
+//  Created by Karn Griffen on 1/2/14.
+//  Copyright (c) 2014 Karn Griffen. All rights reserved.
+//
+
+#import "mainViewController.h"
+#import "sqlite3.h"
+#import "FMDatabase.h"
+
+@interface mainViewController ()
+
+@end
+
+@implementation mainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
